0fb0f5088a9118c0fe8aa1604f14de5a1f8f5440
DaeSung Oh <dae7249@nate.com>
2024-09-07 20:05:56 +0900

    [Fix] lib-cli copy (#20)
    lib-cli copy
    - README.md 도 copy 되도록 수정

--- packages/lib-build/src/util/console.ts
+++ packages/lib-build/src/util/console.ts
@@ -1,7 +1,13 @@
+import chalk from 'chalk'
+
 export const prettierToMultiLineString = (strings: string) => {
   return strings
     .trim()
     .split('\n')
     .map((line) => line.trim())
     .join('\n')
+}
+
+export const consoleSourceToDest = ({ source, dest }: { source: string; dest: string }) => {
+  console.log(chalk.bold.cyanBright(source), chalk.bold('->'), chalk.bold.cyanBright(dest))
 }
